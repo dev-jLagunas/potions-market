5a4e57c8f5d28a12e731d1089fe2036cf046851a
Juan Lagunas <dev.jlagunas@gmail.com>
2024-10-25 17:10:21 +0900

    Updates

--- assets/AboutView-jQFuP0pw.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as c}from"./index-C_zDLt_A.js";const o={};function n(r,s){return c(),t("h1",null,"About test")}const a=e(o,[["render",n]]);export{a as default};
--- assets/FaqsView-CuGlMSWL.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as t}from"./index-C_zDLt_A.js";const s={};function n(o,r){return t(),c("h1",null,"faqs test")}const _=e(s,[["render",n]]);export{_ as default};
