3ea236377e8a842ead6ecb706aa3e745b56a5ad2
Juan Lagunas <dev.jlagunas@gmail.com>
2024-10-25 11:38:08 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <h1>testing one</h1>
+  <h2>testing here</h2>
+  <h3>more testing</h3>
+  <h4>final testing</h4>
+</template>
+
+<style scoped>
+h2 {
+  color: var(--primary-color);
+  font-family: "Lilita One";
+}
+
+h1 {
+  color: red;
+  font-family: "Raleway";
+}
+</style>
